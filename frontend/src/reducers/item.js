f4c59b7e58d85379e46f8e4392c24980d38fb9f4
Manichand Kondapaka <65899331+mani-chand@users.noreply.github.com>
2022-09-13 00:00:09 +0530

    Update item.js

--- frontend/src/reducers/item.js
+++ frontend/src/reducers/item.js
@@ -2,6 +2,7 @@ import { FETCH } from "../constant/itemsActionTypes";
 const item = (items=[],action)=>{
     switch (action.type){
         case FETCH:
+//sort according to time
             return action.payload.sort(
   (objA, objB) => Number(objA.date) - Number(objB.date),
 );
