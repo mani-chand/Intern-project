2d674aa6b70f363cdeeb7eab4250c27035d221c3
Manichand Kondapaka <65899331+mani-chand@users.noreply.github.com>
2022-09-12 23:55:43 +0530

    Update Item.js

--- Backend/models/Item.js
+++ Backend/models/Item.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const Item = new mongoose.Schema({
     name:{type:String},
-    price:{type:Number}
+    price:{type:Number}, 
+    time-slot:{
+             start:{type:Date}, 
+             end:{type:Date}
+           }
 })
 exports.Item = mongoose.model('item',Item)
